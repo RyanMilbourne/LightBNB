660a0c968b65b2d179866d3c5687fe0062643494
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-12-05 15:37:01 -0800

    reformatted

--- 1_queries/4_most_visited_cities.sql
+++ 1_queries/4_most_visited_cities.sql
@@ -1,6 +1,11 @@
-SELECT city,
+SELECT
+  city,
   COUNT(reservations.id) AS total_reservations
-FROM properties
+FROM
+  properties
   JOIN reservations ON properties.id = property_id
-GROUP BY city
-ORDER BY total_reservations DESC;
+GROUP BY
+  city
+ORDER BY
+  total_reservations DESC;
+
