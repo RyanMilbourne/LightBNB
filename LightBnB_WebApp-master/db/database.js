e3071299fcdc105dd64c730607477b1725ba7468
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-12-11 12:50:35 -0800

    add refactor to getAllReservations function

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -80,7 +80,31 @@ const addUser = function(user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT reservations.*, properties.*
+  FROM
+    reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE
+    reservations.guest_id = $1
+  GROUP BY
+    reservations.id,
+    properties.id
+  ORDER BY
+    reservations.start_date
+  LIMIT $2;`, [guest_id, limit])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        return result.rows;
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+      throw err;
+    });
 };
 
 /// Properties
